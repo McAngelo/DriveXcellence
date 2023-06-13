9349d21cfbb63cc6b36e01f373c45c9dcb1c9df1
Michael Johnson <mcangelo200@gmail.com>
2023-06-13 12:58:20 +0000

    --complete work on the dasboard design

---
+++ src/app/layouts/index.ts
@@ -0,0 +1,5 @@
+export * from './auth-layout/auth-layout.component';
+export * from './error-layout/error-layout.component';
+export * from './full/full.component';
+export * from './full/header/header.component';
+export * from './full/sidebar/sidebar.component';
