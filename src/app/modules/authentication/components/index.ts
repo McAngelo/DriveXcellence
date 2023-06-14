c0d8297284af42224641821d6f97bf9aa21e3962
Michael Johnson <mcangelo200@gmail.com>
2023-06-14 07:41:59 +0000

    --worked on the minor changes

---
+++ src/app/modules/authentication/components/index.ts
@@ -0,0 +1,5 @@
+export * from './login/login.component';
+export * from './register/register.component';
+export * from './forgotten-password/forgotten-password.component';
+export * from './verify-account/verify-account.component';
+export * from './reset-password/reset-password.component';
